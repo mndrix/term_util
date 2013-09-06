ce9cdf3f270885285e0fd4b5b3d7168e7667536b
Michael Hendricks <michael@ndrix.org>
2013-09-06 13:16:31 -0600

    Implement code simplification via simplify/2
    simplify/2 allows one to reduce a code structure to its simplest terms.
    It performs some static evaluation and prunes control structures when possible.

--- prolog/term_util.pl
+++ prolog/term_util.pl
@@ -1,6 +1,8 @@
 :- module(term_util, [ control/1
                      , map_args/3
+                     , simplify/2
                      ]).
+:- use_module(library(apply), [maplist/2]).
 :- use_module(library(delay)).
 
 %%  control(+Term) is semidet.
@@ -25,3 +27,87 @@ map_args(Goal, Term1, Term2) :-
     delay(univ(Term1, Functor, Args1)),
     delay(univ(Term2, Functor, Args2)),
     maplist(Goal, Args1, Args2).
+
+
+%%  simplify(+Goal0, -Goal) is det
+%
+%   True if Goal is a simplification of Goal0. Goal0 is typically a
+%   clause body including conjunctions and disjunctions. By evaluating
+%   goals whose value is statically known, we produce a simpler Goal.
+%
+%   For example, the following are all true:
+%
+%       simplify((true,fail,foo),fail).
+%       simplify((X=a,foo(X)), foo(a)).
+%       simplify((true->foo;bar), foo).
+%       simplify((foo,true), foo).
+simplify(Goal0, Goal) :-
+    eval(Goal0, Goal1),
+    !,
+    simplify(Goal1, Goal).
+simplify(Goal, Goal).
+
+
+%%  eval(Goal0, Goal) is semidet
+%
+%   Perform a single static evaluation step. Called repeatedly by
+%   simplify/2 until a fixed point is reached
+
+% rules about disjunction
+eval((fail;Others),Others).  % fail is a disjunctive noop
+
+% rules about ->
+eval((true->Then;_Else), Then). % unreachable Else
+eval((fail->_Then;Else), Else). % unreachable Then
+eval((If->Then;fail), (If->Then)). % Else fails by default
+eval((If->true), once(If)). % using -> as a local cut (aka once/1)
+eval((If0->Then;Else), (If->Then;Else)) :-
+    % evaluable If
+    eval(If0, If).
+eval((If->Then0;Else), (If->Then;Else)) :-
+    % evaluable Then
+    eval(Then0, Then).
+eval((If->Then;Else0), (If->Then;Else)) :-
+    % evaluable If
+    eval(Else0, Else).
+
+% rules about conjunction
+eval((true,Cont),Cont). % true is conjunctive noop
+eval((Goal,true),Goal). % "
+eval((fail,_), fail).  % failure removes continuation
+eval((X=Y,Cont),Cont) :-
+    % perform unification
+    X = Y.
+eval((A,Cont), (B,Cont)) :-  % eval a conjunctive goal
+    eval(A,B).
+eval((A,Cont0),(A,Cont)) :-  % recursively eval conjunction of goals
+    eval(Cont0, Cont).
+
+% goals that can be safely evaluated at compile time
+eval(call(Goal),Goal) :-
+    % call/1 not needed
+    nonvar(Goal).
+eval(Goal,Truth) :-
+    safe_semidet(Goal),
+    ( call(Goal) ->
+        Truth = true
+    ; % otherwise ->
+        Truth = fail
+    ).
+
+
+%%  safe_semidet(Goal) is semidet.
+%
+%   True if Goal can be safely evaluated. The caller can replace it with
+%   either `true` or `fail`.
+safe_semidet(atom_codes(A,C)) :-
+    ( ground(A) ; ground(C) ).
+safe_semidet(Goal) :-
+    Goal =.. [Name|Args],
+    safe_semidet_nonvar(Name),
+    maplist(nonvar, Args).
+
+safe_semidet_nonvar(nonvar).
+safe_semidet_nonvar(integer).
+safe_semidet_nonvar(atom).
+safe_semidet_nonvar(float).
---
+++ t/simplify.pl
@@ -0,0 +1,63 @@
+:- use_module(library(term_util), [simplify/2]).
+
+:- use_module(library(tap)).
+
+% examples from the documentation
+simplify((true,fail,foo),fail).
+simplify((X=a,foo(X)), foo(a)).
+simplify((true->foo;bar), foo).
+simplify((foo,true), foo).
+
+
+% a more complicated example
+simplify(
+    (
+        true,
+        a,
+        X=b,
+        call(X),
+        ( true -> c; nope ),
+        ( fail -> nope; d ),
+        ( fail ; e ),
+        foo(f) = foo(Y),
+        call(Y),
+        nonvar([_]),
+        ( integer(9) -> g ; nope ),
+        atom(42.0),  % fail
+        nope
+    ),
+    (
+        a,
+        b,
+        c,
+        d,
+        e,
+        f,
+        g,
+        fail
+     )
+).
+
+% simplification inside If->Then;Else
+simplify(
+    ( atom(hi) -> integer(9) ; nope ),
+    true
+).
+simplify(
+    ( atom(X) -> integer(9) ; float(hi) ),
+    once(atom(X))
+).
+
+% some predicates that can be evaluated statically
+simplify(
+    ( atom_codes(A,"hi"), write(A) ),
+    write(hi)
+).
+simplify(
+    ( atom_codes(hi,C), write(C) ),
+    write("hi")
+).
+simplify(  % cant' simplify this one
+    ( atom_codes(A,C), write(A), write(C) ),
+    ( atom_codes(A,C), write(A), write(C) )
+).
