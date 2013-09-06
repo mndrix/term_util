739c005f1c384d390628d1fdcd95ef84acc50a2f
Michael Hendricks <michael@ndrix.org>
2013-09-06 10:28:52 -0600

    Add title to pack file

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(term_util).
 version('0.0.1').
-title('').
+title('Predicates for working with terms').
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
